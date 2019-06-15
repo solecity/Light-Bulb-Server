eb317ab6e907ae53aa34bce5b263ead8419913ba
solecity <solecity@hotmail.com>
2019-06-15 14:13:55 +0100

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -10,9 +11,10 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(mainRoutes);
 
-app.listen(port, function () {
-    console.log("Server is running");
+app.listen(port, function (port) {
+    console.log("Server is running. Port: ", port);
 }); 
