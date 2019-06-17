4af05733c87cd13e207e477440b23bb77db9a5ee
solecity <solecity@hotmail.com>
2019-06-17 22:15:11 +0100

    auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,38 @@
 const User = require("../models/user.model.js");
-const jsonMessages = require("../jsonMessages/db.js");
+const jsonMessages = require("../jsonMessages/login.js");
 const bcrypt = require("bcrypt");
 
-/*
+
 async function login(req, res) {
-    const username = ??????? ;
-    const password = ??????? ;
-    let error = "";
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email }).lean();
 
-    if (username == ???? ) {
-        if (password == ????? ) {
+        if (!user) {
+            return res.status(jsonMessages.user.email.status).send(jsonMessages.user.email);
+        }
 
+        const passwordValid = bcrypt.compareSync(password, user.password);
+        
+        if (!passwordValid) {
+            return res.status(jsonMessages.user.password.status).send(jsonMessages.user.password);
         }
-        else {
-            error = "A password não está correta!";
-        }
+
+        res.status(jsonMessages.user.signinSucces.status).send({ msg: jsonMessages.user.signinSucces, user: user });
     }
-    else {
-        error = "O utilizador inserido não existe!";
+    catch (err) {
+        return res.status(jsonMessages.user.error.status).send(jsonMessages.user.error);
     }
-}*/
+}
+
+async function logout(req, res) {
+    req.logout();
+    return res.status(jsonMessages.user.logoutSuccess.status).send(jsonMessages.user.logoutSuccess);
+}
+
+
+// EXPORT ALL FUNCTIONS
+module.exports = {
+    login,
+    logout
+};
